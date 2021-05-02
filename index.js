80a69db8eb019d564f2b5bc9bd6556d5b18fe71d
Paula de Bittencourt <fashion335@gmail.com>
2021-05-02 11:35:26 -0300

    Read and write an ingredient

---
+++ index.js
@@ -0,0 +1,8 @@
+//showing label 'what is in your fridge?"
+const prompt = require('prompt-sync')()
+var ingredient 
+ingredient = prompt("What is in your fridge?")
+console.log(ingredient)
+//ask for an ingredient (user input)
+
+
